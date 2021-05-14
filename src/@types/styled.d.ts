300bc1c7c9b1d77f2d35cc1432739fb9e1da63a4
nicolasdsa <69026498+nicolasdsa@users.noreply.github.com>
2021-05-13 21:54:11 -0300

    Delete src directory

--- src/@types/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components'
-
-import { LightTheme } from '../styles/theme'
-
-export type Theme = typeof LightTheme
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends Theme {}
-}
